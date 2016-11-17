0202b636aa529c1175d3d9243bab6c40e0798dd0
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-11-17 10:06:38 +0100

    feat: Added Common components + created App component linked to our store + created LibraryList connected to store

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+const Spinner = ({ size }) => {
+  return (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
+  );
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
+
+Spinner.propTypes = {
+  size: PropTypes.string,
+};
+
+export { Spinner };
